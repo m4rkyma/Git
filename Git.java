e65afa5d3cbfc281b35ecb574ad5bab10da2b954
Mark <mma1@hwemail.com>
2023-09-07 08:47:49 -0700

    finished code? i think

--- Git.java
+++ Git.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
 public class Git{
+    public HashMap <String, String> blobList = new HashMap<>();
     public void initialize() throws IOException
     {
         if (!(new File ("index").isFile()))
@@ -25,10 +28,22 @@ public class Git{
     }
     public void add (String fileName) throws IOException
     {
+        String s = Blob.Sha1(Blob.getContents(fileName));
+        if (new File ("/Users/markma/Documents/Honors topics/Git/objects/" + s).exists())
+        {
+            return;
+        }
+        if (blobList.containsKey(fileName))
+        {
+            return;
+        }
+        System.out.println ("added " + fileName);
         Blob blob = new Blob (fileName);
-        FileWriter fw = new FileWriter ("index",true);
-        fw.write(fileName + " : " + Blob.Sha1(Blob.getContents(fileName)) + "\n");
-        fw.close();
+        // FileWriter fw = new FileWriter ("index",true);
+        // fw.write(fileName + " : " + Blob.Sha1(Blob.getContents(fileName)) + "\n");
+        // fw.close();
+        blobList.put(fileName, Blob.Sha1(Blob.getContents(fileName)));
+        writeIndex();
         // File index = new File("index");
         // index.createNewFile();
         // if (new File ("index").isFile())
@@ -47,6 +62,18 @@ public class Git{
         String s = Blob.Sha1(Blob.getContents(fileName));
         File f = new File ("/Users/markma/Documents/Honors topics/Git/objects/" + s);
         f.delete();
-
+        blobList.remove(fileName, Blob.Sha1(Blob.getContents(fileName)));
+        writeIndex();
+    }
+    public void writeIndex() throws IOException
+    {
+        FileWriter fw = new FileWriter ("index", true);
+        PrintWriter pw = new PrintWriter (fw);
+        for (String file : blobList.keySet())
+        {
+            pw.write (file + " : " + blobList.get(file) + "\n");
+        }
+        fw.close();
+        pw.close();
     }
 }
--- IndexTester.java
+++ IndexTester.java
@@ -4,10 +4,14 @@ public class IndexTester{
     public static void main (String [] args) throws IOException
     {
         Git git = new Git();
-        git.initialize();
+        // git.initialize();
 
+        // git.add("b.txt");
         git.add("a.txt");
-        git.add("b.txt");
-        git.remove ("a.txt");
+        // git.remove ("a.txt");
+        // git.add("doesn'tmatter.txt");
+        // git.add("doesn'tmatter.txt");
+        // git.remove ("a.txt");
+
     }
 }
