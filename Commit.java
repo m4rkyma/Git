0536b7e630e0864fc188720b78d5ec8517e556a5
sam <baconsamp@gmail.com>
2023-09-22 09:46:57 -0700

    testers

--- Commit.java
+++ Commit.java
@@ -7,58 +7,50 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime; 
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Commit {
 
-
     private String sha, author, desc, treeSha, prevSha, nextSha;
 
-    //This is to commit a file that has the previous sha
-    public Commit(String sha, String author, String desc) throws IOException{
+    // This is to commit a file that has the previous sha
+    public Commit(String sha, String author, String desc) throws IOException {
         prevSha = sha;
         treeSha = treeify();
         this.author = author;
-        this.desc = desc;  
+        this.desc = desc;
     }
 
-    //this is if you do not have the previous sha
-    public Commit(String author, String desc) throws IOException{
+    // this is if you do not have the previous sha
+    public Commit(String author, String desc) throws IOException {
         treeSha = treeify();
         this.author = author;
-        this.desc = desc; 
+        this.desc = desc;
     }
 
-
-    //gets the sha of the tree, pretty damn inportant
-    public String treeify() throws IOException{
+    // gets the sha of the tree, pretty damn inportant
+    public String treeify() throws IOException {
         Tree tree = new Tree();
         return tree.getSha();
     }
 
-
-    //Gets a date (not to homecoming)
-    public String getDate()
-    {
+    // Gets a date (not to homecoming)
+    public String getDate() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         return dtf.format(now);
     }
 
-    //This is actually what saves the file into the object folder, the new commit
-    public void push() throws IOException
-    {
+    // This is actually what saves the file into the object folder, the new commit
+    public void push() throws IOException {
         FileWriter write = new FileWriter(new File("objects/" + sha));
         write.write(treeSha + "\n" + prevSha + "\n" + nextSha + "\n" + author + "\n" + getDate() + "\n" + desc);
         write.close();
     }
 
-
-    //Gets the sha, this is code from blob.java
-    public String getSha()
-    {
+    // Gets the sha, this is code from blob.java
+    public String getSha() {
         String input = (treeSha + "\n" + prevSha + "\n" + author + "\n" + getDate() + "\n" + desc);
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
@@ -69,16 +61,13 @@ public class Commit {
                 hashtext = "0" + hashtext;
             }
             return hashtext;
-        }
-        catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-    //This is the complicated part! Writes the new sha into the old file
-    public void writeInNewCommit() throws IOException 
-    {
+    // This is the complicated part! Writes the new sha into the old file
+    public void writeInNewCommit() throws IOException {
         File orginalFile = new File("objects/" + prevSha);
         File newFile = new File("balls");
         BufferedReader reader = new BufferedReader(new FileReader(orginalFile));
@@ -91,8 +80,8 @@ public class Commit {
                 writer.write(getSha());
             } else
                 writer.write(curr);
-            
-            if(i!=5)
+
+            if (i != 5)
                 writer.write("\n");
             i++;
         }
@@ -101,4 +90,3 @@ public class Commit {
         newFile.renameTo(orginalFile);
     }
 }
-
