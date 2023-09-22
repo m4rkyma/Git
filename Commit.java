a7179d898cab399d945c7d026c09e73a34288142
sqeejee <98850954+sqeejee@users.noreply.github.com>
2023-09-21 20:26:03 -0700

    does this work?

--- Commit.java
+++ Commit.java
@@ -1,5 +1,11 @@
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime; 
@@ -49,13 +55,50 @@ public class Commit {
         write.close();
     }
 
+
+    //Gets the sha, this is code from blob.java
     public String getSha()
     {
-        
+        String input = (treeSha + "\n" + prevSha + "\n" + author + "\n" + getDate() + "\n" + desc);
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
+    //This is the complicated part! Writes the new sha into the old file
+    public void writeInNewCommit() throws IOException 
+    {
+        File orginalFile = new File("objects/" + prevSha);
+        File newFile = new File("balls");
+        BufferedReader reader = new BufferedReader(new FileReader(orginalFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+        String curr;
+        int i = 0;
 
-    
+        while ((curr = reader.readLine()) != null) {
+            if (i == 2) {
+                writer.write(getSha());
+            } else
+                writer.write(curr);
+            
+            if(i!=5)
+                writer.write("\n");
+            i++;
+        }
+        writer.close();
+        reader.close();
+        newFile.renameTo(orginalFile);
+    }
 }
 
